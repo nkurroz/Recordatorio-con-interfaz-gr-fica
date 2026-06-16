b60e1500b9a5915890bea9ce107e1989976d559f
NORAELENA KARELIA URROZ LOPEZ <nkurroz@uamv.edu.ni>
2026-06-16 08:58:14 -0600

    Agregué comentarios para segmentar el código

--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,10 @@ using System;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Interfaz; // Usando el nombre de tu proyecto
+namespace Interfaz; // Usando el nombre del proyecto
 
-// ---------------------------------------------------
-// TU VENTANA PRINCIPAL (Normalita)
-// ---------------------------------------------------
+// Ventana principal simple
+
 public partial class Form1 : Form
 {
     public Form1()
@@ -37,9 +36,8 @@ public partial class Form1 : Form
     }
 }
 
-// ---------------------------------------------------
-// TU VENTANITA EMERGENTE (Estilo Mental OS)
-// ---------------------------------------------------
+// Ventana emergente estilo girly
+
 public class DialogoRecordatorio : Form
 {
     public DialogoRecordatorio()
@@ -51,11 +49,11 @@ public class DialogoRecordatorio : Form
         this.BackColor = Color.FromArgb(255, 230, 240); // Fondo rosa pastel
         this.Text = "Friendly Reminder :D!"; 
 
-        // 2. Imagen del gatito
+        // Imagen del gatito
         try 
         {
             PictureBox pbConejito = new PictureBox();
-            pbConejito.Image = Image.FromFile(@"C:\Users\norae\OneDrive\Imágenes\interfaz\dibujo de gatito tierno.jpg"); // Tu imagen
+            pbConejito.Image = Image.FromFile(@"dibujo de gatito tierno.jpg");
             pbConejito.SizeMode = PictureBoxSizeMode.Zoom;
             pbConejito.Size = new Size(100, 100);
             pbConejito.Location = new Point((this.ClientSize.Width - pbConejito.Width) / 2, 10); 
@@ -68,7 +66,7 @@ public class DialogoRecordatorio : Form
             this.Controls.Add(lblErrorImg);
         }
 
-        // 3. Texto principal
+        //Texto principal
         Label lblMensaje = new Label();
         lblMensaje.Text = "¡Lo mejor está por llegar\nno te rindas! :D";
         lblMensaje.Font = new Font("Lucida Sans Typewriter", 8, FontStyle.Regular); // Letra amigable
@@ -78,7 +76,7 @@ public class DialogoRecordatorio : Form
         lblMensaje.Location = new Point(50, 120);
         this.Controls.Add(lblMensaje);
 
-        // 4. Botón "I believe!"
+        // Botón para cerrar
         Button btnCreer = new Button();
         btnCreer.Text = "✨ Gracias :3 ✨";
         btnCreer.Font = new Font("Lucida Sans Typewriter", 10, FontStyle.Bold);
@@ -87,8 +85,7 @@ public class DialogoRecordatorio : Form
         btnCreer.Size = new Size(180, 50);
         btnCreer.Location = new Point((this.ClientSize.Width - btnCreer.Width) / 2, 180);
         
-        // Al hacer clic, se cierra esta ventanita
-        btnCreer.Click += (s, e) => this.Close(); 
+        btnCreer.Click += (s, e) => this.Close(); // Al hacer clic, se cierra esta ventanita
         
         this.Controls.Add(btnCreer);
     }
